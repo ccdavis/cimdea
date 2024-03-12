c363a08d62e4612f8ac680be8d6464c563016d3a
Colin Davis <ccd.umn.edu>
2024-03-11 22:07:26 -0500

    Stubbing out metadata loading

--- src/conventions.rs
+++ src/conventions.rs
@@ -3,13 +3,56 @@ use crate::ipums_data_model::*;
 use crate::ipums_metadata_model::*;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::path::{Path, PathBuf};
 
-// Key characteristics of collections like all USA Census data, all Time-Use Survey data etc.
+/// Key characteristics of collections like all USA Census data, all Time-Use Survey data etc.
 pub struct MicroDataCollection {
     pub name: String, // Like USA, IPUMSI, ATUS
     pub record_hierarchy: RecordHierarchy,
     pub record_types: HashMap<String, RecordType>, // key is value: 'H', 'P' etc
     pub metadata: Option<MetadataEntities>,
+}
+
+impl MicroDataCollection {
+    /// Read one fixed-width layout file. These files contain some variable level metadata for
+    /// every record type in the data product.
+    pub fn load_metadata_from_layout(&mut self, layout_file: &Path) {}
+
+    /// Read all layout files for the data root like ../output_data/current/layouts
+    /// The existence of a layout file implies existence of a dataset. The presence of
+    /// a variable in a dataset's layout indicates availability in that dataset.
+    pub fn load_metadata_from_all_layouts(&mut self, layouts_dir: &Path) {
+        ()
+    }
+
+    /// The path like ../output_data/current/parquet/us2019a/
+    /// Reading the schema will give approximately the same metadata information
+    /// as reading the fixed-width layout file for the same dataset.
+    pub fn load_metadata_from_parquet(&mut self, parquet_dataset_path: &Path) {}
+
+    /// Takes a path like ../output_data/current/parquet/, which could be derived
+    /// automatically from defaults based on data root or product root. Scans all
+    /// parquet schema information.
+    pub fn load_metadata_from_all_parquet(&mut self, parquet_path: &Path) {}
+
+    /// Load everything available for the selected variables and samples from the available
+    /// metadata database file. Requires 'allow_full_metadata' which depends on a product root
+    /// and a 'metadata.db' file located in the root/metadata/versions location, unless you provide
+    /// a Some(metadata_location).
+    pub fn load_full_metadata_for_selections(
+        &mut self,
+        variables: &Vec<String>,
+        datasets: &Vec<String>,
+        metadata_location: Option<PathBuf>,
+    ) {
+    }
+
+    /// Load all variables and samples for the context and the default metadata location unless
+    /// you provide Some(metadata_location) to override the default. The result of the load may
+    /// be very large, into the gigabyte range.
+    pub fn load_full_metadata(&mut self, metadata_location: Option<PathBuf>) {}
+
+    pub fn clear_metadata(&mut self) {}
 }
 
 pub struct MetadataEntities {
@@ -53,21 +96,27 @@ pub struct Context {
     pub name: String, // A product name like USA, IPUMSI, CPS etc
     // A path name
     // Like /pkg/ipums/usa with ./metadata and ./output_data in it
-    pub product_root: Option<String>,
+    pub product_root: Option<PathBuf>,
     // Any output_data/current path with ./layouts and ./parquet in it
-    pub data_root: Option<String>,
+    pub data_root: Option<PathBuf>,
     pub settings: MicroDataCollection,
+    pub allow_full_metadata: bool,
 }
 
 impl Context {
     // Based on name, use default data root and product root and initialize with defaults
     pub fn default_from_name(name: &str) -> Self {
-        let product_root = format!("/pkg/ipums/{}", &name);
+        let product_root: PathBuf = PathBuf::from(format!("/pkg/ipums/{}", &name));
+        let allow_full_metadata = product_root.exists();
         Self {
             name: name.to_string(),
             product_root: Some(product_root.clone()),
-            data_root: Some(format!("{}/output_data/current", &product_root)),
+            data_root: Some(PathBuf::from(format!(
+                "{}/output_data/current",
+                product_root.to_str().unwrap()
+            ))),
             settings: defaults::defaults_for(name),
+            allow_full_metadata,
         }
     }
 
@@ -79,5 +128,13 @@ impl Context {
 
     pub fn from_name_and_data_root(name: &str, data_root: &str) -> Self {
     }
+
+    // If the context has the project root in addition to the data root it can
+    // attempt to access the metadata DB. Using full metadata requires the
+    // Some(product_root).
+    fn use_full_metadata(&mut self, setting: bool){
+        self.allow_full_metadata = setting;
+    }
+
     */
 }
