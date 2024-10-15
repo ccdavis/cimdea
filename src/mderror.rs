4aa53ee7d29194090b813f4f73b98e376cb3d597
Colin Davis <ccd@umn.edu>
2024-10-15 13:15:54 -0500

    Add the error type

---
+++ src/mderror.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+#[derive(Debug)]
+pub enum MdError {
+    IoError(std::io::Error),
+    Msg(String),
+    // more needed
+}
+
+impl fmt::Display for MdError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Cimdea Error")
+    }
+}
+
+impl std::error::Error for MdError {}
+
+impl From<std::io::Error> for MdError {
+    fn from(err: std::io::Error) -> Self {
+        MdError::IoError(err)
+    }
+}
