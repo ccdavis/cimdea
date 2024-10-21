e62dd9ea2c818641f0e905527f401f46e49e2f03
rileyh <rileyh@umn.edu>
2024-10-21 09:26:18 -0500

    Add serde deserialize definitions for parts of AbacusRequest

--- src/json_schema.rs
+++ src/json_schema.rs
@@ -3,13 +3,28 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-pub struct AbacusRequest {
-    product: String,
-    data_root: String,
-    uoa: String,
-    output_format: String,
-    subpopulation: (),
-    category_bins: (),
-    request_variables: (),
-    request_samples: (),
+pub struct CategoryBin {
+    code: usize,
+    value_label: String,
+    low: usize,
+    high: usize,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RequestVariable {
+    variable_mnemonic: String,
+    mnemonic: String,
+    general_detailed_selection: (),
+    attached_variable_pointer: (),
+    case_selection: bool,
+    request_case_selections: Vec<()>,
+    extract_start: usize,
+    extract_width: usize,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RequestSample {
+    name: String,
+    custom_sampling_ratio: String,
+    first_household_sampled: usize,
 }
