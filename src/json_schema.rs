c70bbba530060ab82374a9587ee208a5df61267b
rileyh <rileyh@umn.edu>
2024-10-21 11:08:36 -0500

    Add a new test request and parse it in a json_schema test

--- src/json_schema.rs
+++ src/json_schema.rs
@@ -1,23 +1,37 @@
 //! Code for modeling and parsing the incoming JSON schema for extract requests
 
+use std::collections::BTreeMap;
+
 use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct AbacusRequest {
+    product: String,
+    data_root: String,
+    uoa: String,
+    output_format: String,
+    subpopulation: Vec<RequestVariable>,
+    category_bins: BTreeMap<String, Vec<CategoryBin>>,
+    request_samples: Vec<RequestSample>,
+    request_variables: Vec<RequestVariable>,
+}
 
 #[derive(Deserialize, Serialize)]
 pub struct CategoryBin {
     code: usize,
     value_label: String,
-    low: usize,
-    high: usize,
+    low: Option<usize>,
+    high: Option<usize>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct RequestVariable {
     variable_mnemonic: String,
     mnemonic: String,
-    general_detailed_selection: (),
+    general_detailed_selection: String,
     attached_variable_pointer: (),
     case_selection: bool,
-    request_case_selections: Vec<()>,
+    request_case_selections: Vec<RequestCaseSelection>,
     extract_start: usize,
     extract_width: usize,
 }
@@ -25,6 +39,29 @@ pub struct RequestVariable {
 #[derive(Deserialize, Serialize)]
 pub struct RequestSample {
     name: String,
-    custom_sampling_ratio: String,
-    first_household_sampled: usize,
+    custom_sampling_ratio: Option<String>,
+    first_household_sampled: Option<usize>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RequestCaseSelection {
+    low_code: String,
+    high_code: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Deserialize a real example AbacusRequest with the request variables
+    /// INCWAGE and MARST.
+    #[test]
+    fn test_deserialize_incwage_marst_example() {
+        let json_str = include_str!("../test/requests/incwage_marst_example.json");
+        let request: AbacusRequest =
+            serde_json::from_str(json_str).expect("should deserialize into an AbacusRequest");
+
+        assert_eq!(request.product, "usa");
+        assert_eq!(request.category_bins["INCWAGE"].len(), 17);
+    }
 }
