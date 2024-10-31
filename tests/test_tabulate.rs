060c8767bfbc1d53d88b6b04dfb252c7bd406793
rileyh <rileyh@umn.edu>
2024-10-31 13:59:48 -0500

    Add a test for tabulate with no category bins, and subpop on a P variable

--- tests/test_tabulate.rs
+++ tests/test_tabulate.rs
@@ -5,7 +5,7 @@ use cimdea::tabulate::tabulate;
 /// This test tabulates a single P variable MARST, which does not have category
 /// bins. There are no subpopulations applied.
 #[test]
-fn test_no_category_bins_no_subpopulations() {
+fn test_no_category_bins_no_subpops() {
     let input_json = include_str!("requests/no_category_bins_no_subpops.json");
     let (ctx, request) =
         AbacusRequest::try_from_json(input_json).expect("should be able to parse input JSON");
@@ -40,6 +40,54 @@ fn test_no_category_bins_no_subpopulations() {
     assert_eq!(table.rows[3][1], "404131");
     assert_eq!(table.rows[4][1], "204365");
     assert_eq!(table.rows[5][1], "1730968");
+
+    // Check MARST
+    assert_eq!(table.rows[0][2], "1");
+    assert_eq!(table.rows[1][2], "2");
+    assert_eq!(table.rows[2][2], "3");
+    assert_eq!(table.rows[3][2], "4");
+    assert_eq!(table.rows[4][2], "5");
+    assert_eq!(table.rows[5][2], "6");
+}
+
+/// This test tabulates the P variable MARST with no category bins. It restricts
+/// to the subpopulation SEX = 2, so just women.
+#[test]
+fn test_no_category_bins_subpop_p_variable() {
+    let input_json = include_str!("requests/no_category_bins_subpop_P_variable.json");
+    let (ctx, rq) =
+        AbacusRequest::try_from_json(input_json).expect("should be able to parse input JSON");
+    let tab = tabulate(&ctx, rq).expect("tabulation should run without errors");
+    let tables = tab.into_inner();
+
+    assert_eq!(tables.len(), 1);
+    let table = tables[0].clone();
+
+    assert_eq!(table.heading.len(), 3);
+    assert_eq!(table.heading[0].name(), "ct");
+    assert_eq!(table.heading[1].name(), "weighted_ct");
+    assert_eq!(table.heading[2].name(), "MARST");
+
+    assert_eq!(table.rows.len(), 6);
+    for row in &table.rows {
+        assert_eq!(row.len(), 3);
+    }
+
+    // Check ct
+    assert_eq!(table.rows[0][0], "5048");
+    assert_eq!(table.rows[1][0], "270");
+    assert_eq!(table.rows[2][0], "432");
+    assert_eq!(table.rows[3][0], "2256");
+    assert_eq!(table.rows[4][0], "1831");
+    assert_eq!(table.rows[5][0], "6622");
+
+    // Check weighted_ct
+    assert_eq!(table.rows[0][1], "496088");
+    assert_eq!(table.rows[1][1], "30965");
+    assert_eq!(table.rows[2][1], "50255");
+    assert_eq!(table.rows[3][1], "240264");
+    assert_eq!(table.rows[4][1], "162628");
+    assert_eq!(table.rows[5][1], "836520");
 
     // Check MARST
     assert_eq!(table.rows[0][2], "1");
