82c55f35760ac1cdae9260dc9ed42c28a31e0a09
rileyh <rileyh@umn.edu>
2024-10-31 15:51:27 -0500

    Improve failure messages for KeyTable::check(), integrate it with a test

--- tests/test_tabulate.rs
+++ tests/test_tabulate.rs
@@ -15,39 +15,18 @@ fn test_no_category_bins_no_subpops() {
     assert_eq!(tables.len(), 1);
     let table = tables[0].clone();
 
-    assert_eq!(table.heading.len(), 3);
-    assert_eq!(table.heading[0].name(), "ct");
-    assert_eq!(table.heading[1].name(), "weighted_ct");
-    assert_eq!(table.heading[2].name(), "MARST");
-
-    assert_eq!(table.rows.len(), 6);
-    for row in &table.rows {
-        assert_eq!(row.len(), 3);
-    }
-
-    // Check ct
-    assert_eq!(table.rows[0][0], "10050");
-    assert_eq!(table.rows[1][0], "499");
-    assert_eq!(table.rows[2][0], "707");
-    assert_eq!(table.rows[3][0], "3670");
-    assert_eq!(table.rows[4][0], "2267");
-    assert_eq!(table.rows[5][0], "13574");
-
-    // Check weighted_ct
-    assert_eq!(table.rows[0][1], "998208");
-    assert_eq!(table.rows[1][1], "54103");
-    assert_eq!(table.rows[2][1], "82407");
-    assert_eq!(table.rows[3][1], "404131");
-    assert_eq!(table.rows[4][1], "204365");
-    assert_eq!(table.rows[5][1], "1730968");
-
-    // Check MARST
-    assert_eq!(table.rows[0][2], "1");
-    assert_eq!(table.rows[1][2], "2");
-    assert_eq!(table.rows[2][2], "3");
-    assert_eq!(table.rows[3][2], "4");
-    assert_eq!(table.rows[4][2], "5");
-    assert_eq!(table.rows[5][2], "6");
+    let key = KeyTable {
+        column_names: ["ct", "weighted_ct", "MARST"],
+        rows: [
+            [10050, 998208, 1],
+            [499, 54103, 2],
+            [707, 82407, 3],
+            [3670, 404131, 4],
+            [2267, 204365, 5],
+            [13574, 1730968, 6],
+        ],
+    };
+    key.check(&table);
 }
 
 /// This test tabulates the P variable MARST with no category bins. It restricts
@@ -119,25 +98,50 @@ impl<'a, const W: usize, const H: usize> KeyTable<'a, W, H> {
     }
 
     fn check_heading(&self, table: &Table) {
-        assert_eq!(table.heading.len(), W);
+        let num_table_headers = table.heading.len();
+        assert_eq!(
+            num_table_headers, W,
+            "number of columns in heading differs: key has {W}, \
+            table has {num_table_headers}"
+        );
         for index in 0..W {
-            assert_eq!(table.heading[index].name(), self.column_names[index]);
+            let table_column_name = table.heading[index].name();
+            let key_column_name = self.column_names[index];
+            assert_eq!(
+                table_column_name, key_column_name,
+                "name of column {index} differs: key has '{key_column_name}', \
+                table has '{table_column_name}'"
+            );
         }
     }
 
     fn check_row_dimensions(&self, table: &Table) {
-        assert_eq!(table.rows.len(), H);
-        for row in &table.rows {
-            assert_eq!(row.len(), W);
+        let num_table_rows = table.rows.len();
+
+        assert_eq!(
+            num_table_rows, H,
+            "number of rows differs: key has {H}, table has {num_table_rows}"
+        );
+        for index in 0..H {
+            let row_len = table.rows[index].len();
+            assert_eq!(
+                row_len, W,
+                "length of row {index} differs: key has {W}, table has {row_len}"
+            );
         }
     }
 
     fn check_row_entries(&self, table: &Table) {
         for column_index in 0..W {
+            let column_name = self.column_names[column_index];
             for row_index in 0..H {
                 let key_entry = self.rows[row_index][column_index].to_string();
                 let table_entry = &table.rows[row_index][column_index];
-                assert_eq!(&key_entry, table_entry);
+                assert_eq!(
+                    &key_entry, table_entry,
+                    "entry in column {column_index} ('{column_name}') and row \
+                    {row_index} differs: key has {key_entry}, table has {table_entry}"
+                );
             }
         }
     }
