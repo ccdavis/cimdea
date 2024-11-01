a5e70c9d0586a0dc03908da129eb6eb5c5ab89dd
rileyh <rileyh@umn.edu>
2024-11-01 12:35:43 -0500

    Add a test that hits the bug
    Tabulation runs without errors for this request. But if you try to serialize it
    to JSON, it returns an error. It seems like somehow we're not passing around
    extract_width correctly, and then we need it to format the results in JSON. The
    tabulation serializes to text without errors.

---
+++ tests/test_serialize_tabulation.rs
@@ -0,0 +1,17 @@
+use cimdea::request::{AbacusRequest, DataRequest};
+use cimdea::tabulate::{self, TableFormat};
+
+/// Computes and serializes into JSON a tabulation on the EDUC variable, with
+/// general width 2 and detailed width 3.
+#[test]
+fn test_serialize_general_detailed_to_json() {
+    let input_json = include_str!("requests/educ_general_detailed_selection.json");
+    let (ctx, rq) =
+        AbacusRequest::try_from_json(input_json).expect("should be able to deserialize input JSON");
+    let tab = tabulate::tabulate(&ctx, rq).expect("tabulation should run without errors");
+    let output_json = tab
+        .output(TableFormat::Json)
+        .expect("tabulation should serialize into JSON");
+    let _: serde_json::Value =
+        serde_json::from_str(&output_json).expect("serialized tabulation should be valid JSON");
+}
