cba3754649d90da71e46ad65f644980abea06521
rileyh <rileyh@umn.edu>
2024-11-12 14:52:58 -0600

    Remove old doc directory
    This is now dynamically created and uploaded by GitHub Actions. We don't have
    to keep it in the repo anymore.

--- doc/src-files.js
+++
@@ -1,4 +0,0 @@
-var srcIndex = new Map(JSON.parse('[\
-["cimdea",["",[],["conventions.rs","defaults.rs","ipums_data_model.rs","ipums_metadata_model.rs","layout.rs","lib.rs","query_gen.rs","request.rs"]]]\
-]'));
-createSrcSidebar();
