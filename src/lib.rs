8a941e5309884fdc4908a08367d8cf7fdbeface1
rileyh <rileyh@umn.edu>
2024-11-12 16:45:28 -0600

    Start a rough draft of some crate docs in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,53 @@
-//! CIMDEA == Convenient IPUMS Microdata Extraction and Aggregation
+//! # CIMDEA: Convenient IPUMS Microdata Extraction and Aggregation
+//!
+//! ## Computing a Tabulation
+//!
+//! The simplest way to create a tabulation request is with the aptly named
+//! [SimpleRequest](request::SimpleRequest). The [from_names](request::DataRequest::from_names)
+//! function supports creating a request from product, dataset, and variable names. `from_names`
+//! returns a context with loaded metadata and a `SimpleRequest`.
+//!
+//! ```
+//! use cimdea::request::{DataRequest, SimpleRequest};
+//!
+//! # let data_root = "tests/data_root/".to_string();
+//! // Set data_root to the directory with your input data.
+//! // let data_root: String = ...;
+//! let (ctx, rq) = SimpleRequest::from_names(
+//!     "usa",
+//!     &["us2015b"],
+//!     &["MARST"],
+//!     None,
+//!     None,
+//!     Some(data_root),
+//! ).unwrap();
+//! ```
+//!
+//! Once you have a [Context](conventions::Context) and a type like `SimpleRequest` which
+//! implements [DataRequest](request::DataRequest), you can pass them to the
+//! [tabulate](tabulate::tabulate) function to compute the tabulation which the `DataRequest`
+//! defines.
+//!
+//! ```
+//! # use cimdea::request::{DataRequest, SimpleRequest};
+//! use cimdea::tabulate::{self, TableFormat};
+//! # let data_root = "tests/data_root/".to_string();
+//! # let (ctx, rq) = SimpleRequest::from_names(
+//! #     "usa",
+//! #     &["us2015b"],
+//! #     &["MARST"],
+//! #     None,
+//! #     None,
+//! #     Some(data_root),
+//! # ).unwrap();
+//! 
+//! let tab = tabulate::tabulate(&ctx, rq).unwrap();
+//! let json = tab.output(TableFormat::Json).unwrap();
+//! ```
+//!
+//! For more complex requests which need to use features like general versions of
+//! variables, subpopulations, or category bins, please see
+//! [AbacusRequest](request::AbacusRequest), which also implements `DataRequest`.
 
 pub mod conventions;
 pub mod defaults;
